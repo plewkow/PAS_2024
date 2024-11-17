75a0f8e7b879d08f93fa705dd8cf01516580395b
Bartosz Podemski <bartekpodemski@gmail.com>
2024-11-17 15:34:34 +0100

    Implement mappings for Users

--- REST/src/main/java/pl/lodz/p/edu/rest/mapper/UserMapper.java
+++ REST/src/main/java/pl/lodz/p/edu/rest/mapper/UserMapper.java
@@ -2,8 +2,7 @@ package pl.lodz.p.edu.rest.mapper;
 
 import pl.lodz.p.edu.rest.dto.ClientDTO;
 import pl.lodz.p.edu.rest.dto.UserDTO;
-import pl.lodz.p.edu.rest.model.user.Client;
-import pl.lodz.p.edu.rest.model.user.User;
+import pl.lodz.p.edu.rest.model.user.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,7 +14,7 @@ public class UserMapper {
                 .collect(Collectors.toList());
     }
 
-    private UserDTO convertToDTO(User user) {
+    public UserDTO convertToDTO(User user) {
         if (user instanceof Client client) {
             return new ClientDTO(
                     client.getLogin(),
@@ -34,5 +33,36 @@ public class UserMapper {
                     user.getRole()
             );
         }
+    }
+
+    public UserDTO convertToUserDTO(User user) {
+        return new UserDTO(
+                user.getLogin(),
+                user.getPassword(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getRole()
+        );
+    }
+
+    public User convertToUser(UserDTO userDTO) {
+        return switch (userDTO.getRole()) {
+            case CLIENT -> new Client(
+                    userDTO.getLogin(),
+                    userDTO.getPassword(),
+                    userDTO.getFirstName(),
+                    userDTO.getLastName(),
+                    ClientType.createNoMembership());
+            case ADMIN -> new Admin(
+                    userDTO.getLogin(),
+                    userDTO.getPassword(),
+                    userDTO.getFirstName(),
+                    userDTO.getLastName());
+            case MANAGER -> new Manager(
+                    userDTO.getLogin(),
+                    userDTO.getPassword(),
+                    userDTO.getFirstName(),
+                    userDTO.getLastName());
+        };
     }
 }
