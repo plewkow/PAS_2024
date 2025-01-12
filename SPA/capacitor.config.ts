4a2f215b0a53c59eade3a0dbc097cc98da930f9f
Bartosz Podemski <bartekpodemski@gmail.com>
2025-01-12 18:26:04 +0100

    mobile app

---
+++ SPA/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'spa',
+  webDir: 'dist',
+  bundledWebRuntime: true
+};
+
+export default config;
