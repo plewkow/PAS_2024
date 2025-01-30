f66aa8f44815bfce1e3550aa0eeb49a6511e0bd6
Bartosz Podemski <bartekpodemski@gmail.com>
2025-01-30 20:37:23 +0100

    Fix change password

--- SPA/src/components/ChangePasswordDialog.tsx
+++ SPA/src/components/ChangePasswordDialog.tsx
@@ -12,39 +12,76 @@ import { Input } from "@/components/ui/input";
 import { useToast } from "@/hooks/use-toast";
 import { useState } from "react";
 import apiClient from "@/lib/apiClient";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 interface ChangePasswordDialogProps {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+const formSchema = z
+  .object({
+    currentPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(
+        /[@$!%*?&]/,
+        "Password must contain at least one special character (@, $, !, %, *, ?, &)"
+      ),
+    newPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(
+        /[@$!%*?&]/,
+        "Password must contain at least one special character (@, $, !, %, *, ?, &)"
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords must match",
+  });
+
 const ChangePasswordDialog: FC<ChangePasswordDialogProps> = ({
   isOpen,
   setIsOpen,
 }) => {
   const { toast } = useToast();
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  // const [currentPassword, setCurrentPassword] = useState("");
+  // const [newPassword, setNewPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      currentPassword: "",
+      newPassword: "",
+      confirmPassword: "",
+    },
+  });
 
-  const handlePasswordChange = async () => {
-    if (newPassword !== confirmPassword) {
-      toast({
-        title: "Error",
-        description: "New passwords do not match",
-        variant: "destructive",
-      });
-      return;
-    }
-
-    setLoading(true);
-
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const { confirmPassword, ...data } = values;
     try {
-      await apiClient.put("/users/me/changePassword", {
-        currentPassword,
-        newPassword,
-      });
+      setLoading(true);
+      await apiClient.put("/users/me/changePassword", data);
 
       toast({
         title: "Success",
@@ -53,9 +90,7 @@ const ChangePasswordDialog: FC<ChangePasswordDialogProps> = ({
       });
 
       setIsOpen(false);
-      setNewPassword("");
-      setCurrentPassword("");
-      setConfirmPassword("");
+      form.reset();
     } catch (error) {
       toast({
         title: "Error",
@@ -63,10 +98,50 @@ const ChangePasswordDialog: FC<ChangePasswordDialogProps> = ({
         description: error.response.data || "Failed to change password",
         variant: "destructive",
       });
+    } finally {
+      setLoading(false);
     }
-
-    setLoading(false);
   };
+
+  // const handlePasswordChange = async () => {
+  //   if (newPassword !== confirmPassword) {
+  //     toast({
+  //       title: "Error",
+  //       description: "New passwords do not match",
+  //       variant: "destructive",
+  //     });
+  //     return;
+  //   }
+
+  //   setLoading(true);
+
+  //   try {
+  //     await apiClient.put("/users/me/changePassword", {
+  //       currentPassword,
+  //       newPassword,
+  //     });
+
+  //     toast({
+  //       title: "Success",
+  //       description: "Password changed successfully!",
+  //       variant: "success",
+  //     });
+
+  //     setIsOpen(false);
+  //     setNewPassword("");
+  //     setCurrentPassword("");
+  //     setConfirmPassword("");
+  //   } catch (error) {
+  //     toast({
+  //       title: "Error",
+  //       // @ts-ignore
+  //       description: error.response.data || "Failed to change password",
+  //       variant: "destructive",
+  //     });
+  //   }
+
+  //   setLoading(false);
+  // };
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -77,8 +152,58 @@ const ChangePasswordDialog: FC<ChangePasswordDialogProps> = ({
             Enter your current and new password
           </DialogDescription>
         </DialogHeader>
-        <div className="space-y-2">
-          <Input
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="currentPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Current Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Current Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="newPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>New Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="New Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Confirm Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="w-full flex justify-between">
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">
+                {loading ? "Changing..." : "Confirm"}
+              </Button>
+            </div>
+          </form>
+        </Form>
+        {/* <Input
             type="password"
             placeholder="Current Password"
             value={currentPassword}
@@ -95,16 +220,7 @@ const ChangePasswordDialog: FC<ChangePasswordDialogProps> = ({
             placeholder="Confirm New Password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
-        <div className="flex justify-between mt-4">
-          <DialogClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DialogClose>
-          <Button onClick={handlePasswordChange} disabled={loading}>
-            {loading ? "Changing..." : "Confirm"}
-          </Button>
-        </div>
+          /> */}
       </DialogContent>
     </Dialog>
   );
